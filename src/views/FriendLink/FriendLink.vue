31f2574b69a403666d42d9ca073c4d253c9a2dc4
amadeus <1685736247@qq.com>
2026-01-08 13:11:47 +0800

    feat: 留言板功能基本正常工作

--- src/views/FriendLink/FriendLink.vue
+++ src/views/FriendLink/FriendLink.vue
@@ -1,95 +1,19 @@
-<!--<script setup>-->
+<script setup>
 
-<!--</script>-->
-
-<!--<template>-->
-<!--  <div class="friendLink-page">-->
-
-<!--  </div>-->
-<!--</template>-->
-
-<!--<style scoped>-->
-<!--  .friendLink-page{-->
-<!--    background: black url('@/assets/background/bg_01.jpg') no-repeat center top;-->
-<!--    min-height: 100vh;-->
-<!--  }-->
-<!--</style>-->
-
-<template>
-  <el-upload
-      class="avatar-uploader"
-      action="http://localhost:8080/upload"
-      :show-file-list="false"
-      :on-success="handleAvatarSuccess"
-      :before-upload="beforeAvatarUpload"
-      :headers="{token:token}"
-  >
-    <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-  </el-upload>
-</template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-import { ElMessage } from 'element-plus'
-import { Plus } from '@element-plus/icons-vue'
-
-import type { UploadProps } from 'element-plus'
-
-//el-upload 组件的 action 属性默认使用浏览器原生的上传行为，不会自动使用你项目中配置的 Axios 拦截器（比如自动添加 Token）。
-//获取token,让el-upload组件携带token上传图片
-const token = localStorage.getItem('token')
-
-const imageUrl = ref('')
-
-const handleAvatarSuccess: UploadProps['onSuccess'] = (
-    response,
-    uploadFile
-) => {
-  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
-}
-
-const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-  if (rawFile.type !== 'image/jpeg') {
-    ElMessage.error('头像必须是 JPG 格式~')
-    console.log(111)
-    return false
-  } else if (rawFile.size / 1024 / 1024 > 2) {
-    ElMessage.error('头像的大小不能超过2MB~')
-    console.log(222)
-    return false
-  }
-  return true
-}
 </script>
 
+<template>
+  <div class="friendLink-page">
+
+  </div>
+</template>
+
 <style scoped>
-.avatar-uploader .avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
-}
+  .friendLink-page{
+    background: black url('@/assets/background/bg_01.jpg') no-repeat center top;
+    min-height: 100vh;
+  }
 </style>
 
-<style>
-.avatar-uploader .el-upload {
-  border: 1px dashed var(--el-border-color);
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  transition: var(--el-transition-duration-fast);
-}
 
-.avatar-uploader .el-upload:hover {
-  border-color: var(--el-color-primary);
-}
 
-.el-icon.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  text-align: center;
-}
-</style>
--- src/views/MessageBoard/Component/MessageEdit.vue
+++ src/views/MessageBoard/Component/MessageEdit.vue
@@ -1,5 +1,7 @@
-<script setup>
+<script lang="ts" setup>
   import {ref, watch} from "vue";
+  import {Plus} from "@element-plus/icons-vue";
+  import { ElMessage } from 'element-plus'
 
   const emit = defineEmits(['on-submit'])
   const props = defineProps({
@@ -27,6 +29,34 @@
     // 把数据回传给父组件
     emit('on-submit', commentData.value);
   }
+
+
+  //上传图片
+  
+  //el-upload 组件的 action 属性默认使用浏览器原生的上传行为，不会自动使用你项目中配置的 Axios 拦截器（比如自动添加 Token）。
+  //获取token,让el-upload组件携带token上传图片
+  const token = localStorage.getItem('token')
+
+
+
+  const handleAvatarSuccess: UploadProps['onSuccess'] = (
+      response,
+      uploadFile
+  ) => {
+    // 修复：commentData 是一个 ref，必须通过 .value 访问其内部对象的属性
+    commentData.value.iconurl = URL.createObjectURL(uploadFile.raw!)
+  }
+
+  const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+    if (rawFile.type !== 'image/jpeg') {
+      ElMessage.error('头像必须是 JPG 格式~')
+      return false
+    } else if (rawFile.size / 1024 / 1024 > 2) {
+      ElMessage.error('头像的大小不能超过2MB~')
+      return false
+    }
+    return true
+  }
 </script>
 
 <template>
@@ -38,8 +68,19 @@
       <el-form-item label="留言内容">
         <el-input v-model="commentData.content" placeholder="请输入留言内容"></el-input>
       </el-form-item>
-      <el-form-item label="上传文件">
-        <el-input v-model="commentData.iconurl" placeholder="上传头像url"></el-input>
+      <el-form-item label="上传头像">
+        <el-upload
+            class="avatar-uploader"
+            action="http://localhost:8080/upload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+            :headers="{token:token}"
+        >
+          <!-- 修复：在模板中 Vue 会自动解包 ref，因此直接使用 commentData.iconurl 即可 -->
+          <img v-if="commentData.iconurl" :src="commentData.iconurl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onsubmit">提交</el-button>
@@ -48,6 +89,35 @@
   </el-dialog>
 </template>
 
-<style scoped>
 
+
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
 </style>
